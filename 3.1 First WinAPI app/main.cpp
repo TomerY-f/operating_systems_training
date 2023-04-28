c52206fb40af9c0afad306fd79748a10ceef6362
Tomer <tomeryehezqel@gmail.com>
2023-04-28 19:55:18 +0300

    CreateFile and ReadFile with WinAPI.

--- 3.1 First WinAPI app/main.cpp
+++ 3.1 First WinAPI app/main.cpp
@@ -4,39 +4,48 @@
 WCHAR input_file[9] = L"test.txt";
 LPCWSTR p_input_file = input_file;
 
-#define BUFFER_SIZE 20
+#define BUFFER_SIZE 30
 
 int main()
 {
-    HANDLE hFile;
+    HANDLE hFile = NULL;
     CHAR inBuffer[BUFFER_SIZE];
     LPVOID pbuffer = inBuffer;
     DWORD nNumberOfBytesToRead = BUFFER_SIZE;
+    DWORD nNumberOfBytesRead;
 
-    hFile = CreateFile(input_file,               // file to open
-        GENERIC_READ,          // open for reading
-        FILE_SHARE_READ,       // share for reading
-        NULL,                  // default security
-        OPEN_EXISTING,         // existing file only
-        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, // normal file
-        NULL);                 // no attr. template
+    hFile = CreateFile(input_file,               
+        GENERIC_READ,          
+        FILE_SHARE_READ,       
+        NULL,                  
+        OPEN_EXISTING,         
+        FILE_ATTRIBUTE_NORMAL, 
+        NULL);                 
+
+    int error_handle_code = GetLastError();
+    if (error_handle_code != 0)
+    {
+        printf("Handle is loaded in invalid way. Error code: %d\n", error_handle_code);
+        return 1;
+    }
 
     BOOL reading_result = ReadFile(hFile,
         pbuffer,
         nNumberOfBytesToRead,
-        NULL,
+        &nNumberOfBytesRead,
         NULL);
 
-    if (reading_result == TRUE)
+    if (!reading_result)
     {
-        printf("Work properly. The text is: %s \n", inBuffer);
+        printf("Worked wrong. error code: %d. \n", GetLastError());
+        return 1;
     }
-    else
-    {
-        printf("Work wrong. error code: %d. \n", GetLastError());
-    }
-    
+
+    printf("Work properly. The text is: %.*s \n", (int)nNumberOfBytesRead, inBuffer);
+
 
     CloseHandle(hFile);
+    return 0;
 }
 
+int main();

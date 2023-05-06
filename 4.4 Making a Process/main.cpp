1c5b6fd8fee66d6113cb2cfe3d0805e278cb07e8
Tomer <tomeryehezqel@gmail.com>
2023-05-06 16:44:14 +0300

    First CreateProcessA implementation.

--- 4.4 Making a Process/main.cpp
+++ 4.4 Making a Process/main.cpp
@@ -1,21 +1,46 @@
 #include <Windows.h>
 #include <stdio.h>
-#include <iostream>
 
-// argc - argument count - means number of arguments the function recevies. 
-// aegv - argument vector - an array of pointers to arguments.
+#define EXE_FILENAME "C:\\Users\\xbont\\source\\repos\\operating_systems_training\\4.4 Making a Process\\x64\\Hello_World.exe"
+#define PROCESS_ARG "4"
 
-// In the example below, it can be shown in debugging that argc will include two arguments for typing one input for the exe - one will be the value of the argument, 
-//																															  and the other will be the address of the argument.
-
-
-int main(int argc, char *argv[])
+int main()
 {
-	if (argc != 2) {
-		printf("Incorrect number of arguments\n");
-		return 0;
+	STARTUPINFOA si; //Default Startup info for CreateProcessA.
+	PROCESS_INFORMATION pi; //Default Process info for CreateProcessA.
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	// create argument string
+	CHAR exe_filename[] = EXE_FILENAME;
+	CHAR process_arg[] = PROCESS_ARG;
+	INT size = strlen(exe_filename) + strlen(process_arg) + 2;
+	PCHAR param = (PCHAR)malloc(size * sizeof(CHAR)); //Allocate memory for command line parameter for CreateFileA.
+	sprintf_s(param, size, "%s %s", exe_filename, process_arg); //String command line parameter.
+	
+	// Start the child process.
+	BOOL success_flag = TRUE;
+	success_flag = CreateProcessA(NULL,
+		param, // Command line
+		NULL, // Process handle not inheritable
+		NULL, // Thread handle not inheritable
+		FALSE, // Set handle inheritance to FALSE
+		0, // No creation flags
+		NULL, // Use parent's environment block
+		NULL, // Use parent's starting directory
+		&si, // Pointer to STARTUPINFO structure
+		&pi); // Pointer to PROCESS_INFORMATION structure
+	
+	// Close Handels and free allocation
+	CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+	free(param);
+
+	if (!success_flag) {
+		printf("False to Create the Process. Error code: %d\n", GetLastError());
 	}
-	int id = atoi(argv[1]);
-	printf("Hello World! Process got argument %d\n", id);
-	return 1;
+
+	return 0;
 }
